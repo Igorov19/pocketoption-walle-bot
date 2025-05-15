5f120d0a461414cdf1756c2c7547d2d182dad4e1
Igorov19 <igorigorov835@gmail.com>
2025-05-15 18:57:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-# Основний код бота
-print('Bot запущено')
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
+
+bot = Bot(token=os.getenv("ТОКЕН"))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton("Купити"), types.KeyboardButton("Продати"))
+    await message.answer("Бот активний! Обери дію:", reply_markup=keyboard)
+
+@dp.message_handler(lambda message: message.text == "Купити")
+async def buy_handler(message: types.Message):
+    await message.answer("Команда 'Купити' прийнята!")
+
+@dp.message_handler(lambda message: message.text == "Продати")
+async def sell_handler(message: types.Message):
+    await message.answer("Команда 'Продати' прийнята!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
