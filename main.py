26a4154e62ca24826ed7b7c85a9b17c857b99b91
Igorov19 <igorigorov835@gmail.com>
2025-05-15 19:03:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, types, executor
 import os
 
 bot = Bot(token=os.getenv("ТОКЕН"))
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Купити"), types.KeyboardButton("Продати"))
-    await message.answer("Бот активний! Обери дію:", reply_markup=keyboard)
+    keyboard.add("Купити", "Продати")
+    await message.answer("Обери дію:", reply_markup=keyboard)
 
-@dp.message_handler(lambda message: message.text == "Купити")
-async def buy_handler(message: types.Message):
-    await message.answer("Команда 'Купити' прийнята!")
+@dp.message_handler(lambda message: message.text in ["Купити", "Продати"])
+async def trade_handler(message: types.Message):
+    if message.text == "Купити":
+        await message.answer("Виконую купівлю...")
+    else:
+        await message.answer("Виконую продаж...")
 
-@dp.message_handler(lambda message: message.text == "Продати")
-async def sell_handler(message: types.Message):
-    await message.answer("Команда 'Продати' прийнята!")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
